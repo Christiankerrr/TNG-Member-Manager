cd22fe142ec6c4a9431a7814cf7d87757cfc2be0
MattWasTaken1 <mkart1013@gmail.com>
2025-03-17 16:38:04 -0400

    - Created bot task loops

--- Bot.py
+++ Bot.py
@@ -1,12 +1,24 @@
-from discord.ext import commands
+from discord.ext import commands, tasks
 
-class BotClient(commands.Bot):
+class BotClient (commands.Bot):
+
+	@tasks.loop(hours = 24)
+	def update_member_db(self):
+
+		pass
+
+	@tasks.loop(hours = 24)
+	def prune_events(self):
+
+		pass
 
 	def __init__(self, *args, **kwargs):
 
 		super().__init__(*args, **kwargs)
 
 		self.strip_after_prefix = True
+
+		self.activeEvents = []
 
 	# def command(self, *args, **kwargs):
 	#
