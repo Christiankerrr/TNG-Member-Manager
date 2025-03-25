16bde085323ff800ac6c7b7d540d421176384c8d
MattWasTaken1 <mkart1013@gmail.com>
2025-03-25 14:12:57 -0400

    - Added code to run bot

--- Main.py
+++ Main.py
@@ -9,8 +9,7 @@ from Commands import bot
 from Event import start_event, end_event, start_meeting
 from Member import Member
 
-# with open("Token.txt") as file:
-#
-# 	token = file.readline()
-# 	bot.run(token)
+with open("Token.txt") as file:
 
+	token = file.readline()
+	bot.run(token)
