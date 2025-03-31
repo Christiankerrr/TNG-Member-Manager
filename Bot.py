a0c284627b076ad403323574f191e64971f279df
MattWasTaken1 <mkart1013@gmail.com>
2025-03-31 16:42:01 -0400

    - Added update_member_db()

--- Bot.py
+++ Bot.py
@@ -1,16 +1,26 @@
 from discord.ext import commands, tasks
 
+from DB_Manage import get_loop_vals, get_all_id, get_status, edit_attr
+
 class BotClient (commands.Bot):
 
 	@tasks.loop(hours = 24)
 	async def update_member_db(self):
 
-		pass
+		tngServer = self.get_guild(self.tngServerID)
 
-		# For each member:
-		# Update active status
-		# Update tag
-		# Update name
+		totalEventHours, totalMeetings = get_loop_vals()
+
+		for id in get_all_id():
+
+			memberAttrs = get_status(id)
+
+			memberObj = tngServer.fetch_member(id)
+			edit_attr("tag", memberObj.name)
+			edit_attr("name", memberObj.nick)
+
+			isActive = (memberAttrs["hours"] >= totalEventHours/2) and (memberAttrs["meeting"] >= totalMeetings/2)
+			edit_attr("isActive", isActive)
 
 	@tasks.loop(hours = 24)
 	async def prune_events(self):
@@ -22,6 +32,8 @@ class BotClient (commands.Bot):
 		super().__init__(*args, **kwargs)
 
 		self.strip_after_prefix = True
+
+		self.tngServerID = 1014692801281273868
 
 		self.activeEvents = []
 		self.registrationInfo = {}
