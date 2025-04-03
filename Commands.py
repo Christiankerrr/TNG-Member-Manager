7c94b360d5bdae6708793787ab69ccaf5a8d208f
JHeinis <j.t.heinis@gmail.com>
2025-04-03 11:30:26 -0800

    notes to edit

--- Commands.py
+++ Commands.py
@@ -20,6 +20,13 @@ bot = BotClient(command_prefix = "?", intents = discord.Intents.all())
 	## show leaderboard --- I
 	## manually edit data --- U
 	## display all commands --- I
+	## edit event - by event name
+	## edit meeting (perform any logic necessary: start time, end time)
+	## delete member/delete event - by event/member name
+
+	## before invoke:
+		## add unrecognized members to database - identify tag and recognize
+
 
 
 ## Tested-Working Commands
@@ -56,6 +63,8 @@ async def member_status(ctx, tag):
 @bot.command()
 async def event_registration(ctx, eventName, eventDur):
 
+	# create blank event, send to aaron
+
 	# Call UI function to display event registration info
 	ui_func_EventRegistrDisplay(eventName, eventDur)
 	pass
@@ -64,6 +73,7 @@ async def event_registration(ctx, eventName, eventDur):
 @bot.command()
 async def start_event(ctx, eventName):
 
+	# 
 	# Call UI function to end event registration function
 	ui_func_EndRegistration()
 	await ctx.send("Event registration has ended, thank you for your responses!")
@@ -75,6 +85,9 @@ async def start_event(ctx, eventName):
 ## Start Meeting
 @bot.command()
 async def start_meeting(ctx, *args):
+
+	startTime = current.time
+	# add to db
 
 	# Call UI function to start an event with the sign in/out buttons, doesn't need special name
 	ui_func_StartMeeting()
@@ -125,25 +138,4 @@ async def help(ctx):
 	
 	# find a way to itemize and display commands...?
 	# I don't think i can call a command ti just display it. this seems common enough that I could youtube it though
-
-##Multi-argument Ping-Pong example w/ ChatGPT [Don't need, just to help visualize multiple arguments]
-# @bot.command()
-# async def ping(ctx, *args):
-#     if not args:  # If no arguments are provided
-#         await ctx.send("❗ Please provide at least one argument (fast, slow, normal).")
-#         return
-
-#     async def send_pong(arg):
-#         arg = arg.lower()
-#         if arg == "fast":
-#             await ctx.send("⚡ Fast Pong!")
-#         elif arg == "slow":
-#             await ctx.send("🐢 Slow Pong...")
-#         elif arg == "normal":
-#             await ctx.send("🏓 Regular Pong!")
-#         else:
-#             await ctx.send("❓ Unknown Pong Type")
-
-#     # Send a response for each argument
-#     for arg in args:
-#         await send_pong(arg)
+	pass
