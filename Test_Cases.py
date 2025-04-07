e7ead546ecf4b3a7f781c06d28226b4cedd69a0c
aafanjoy <fanjoya1@my.erau.edu>
2025-04-07 16:10:24 -0400

    read write SQL database

---
+++ Test_Cases.py
@@ -0,0 +1,19 @@
+# Touch-N-Go Member Manager
+# Adrian Fanjoy
+# Test cases
+
+import DB_Manage
+
+# Create database
+def TNG_0():
+    DB_Manage.create_database()
+
+# Run TNG_1
+
+# Menu
+choice = input("Choose from the following options:")
+print("0. Create database")
+print("1. TNG_1")
+print("5. Exit")
+
+# Switch case
