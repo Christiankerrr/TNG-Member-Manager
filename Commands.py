896372eb97311c7ecf52a6ff13b3e6f08ea50865
JHeinis <j.t.heinis@gmail.com>
2025-04-08 12:44:27 -0800

    updated with DBManage function names

--- Commands.py
+++ Commands.py
@@ -26,11 +26,22 @@ bot = BotClient(command_prefix = "?", intents = discord.Intents.all())
 	## edit meeting (perform any logic necessary: start time, end time) --- U
 	## delete member --- U
 	## delete event --- U
+
 	## extra dm to admin to verify sign in?
 
-	## before invoke:
-		## add unrecognized members to database - identify tag and recognize
 
+## Before Invoke
+# Meant to add anyone not currently in the DB to the DB
+# Additionally check permissions??
+@bot.check
+async def before_command(context):
+
+	await bot.wait_until_ready()
+	if DB_Manage.locate_member(context.author.id) == False:
+		DB_Manage.write_member(context.author.id, context.author)
+
+	# if not isinstance(bot.userDB[context.author.id], context.command.permissions):
+    #     await context.send(f"Sorry, you don't have the valid permissions to run that command. This command can only be run by Bot {context.command.permissions.ranking}s and above.")
 
 
 ## Tested-Working Commands
@@ -45,32 +56,46 @@ async def print_db(ctx):
 @bot.command()
 async def write_member(ctx, id, attr, name):
 
-	# make it take just the id -> use discord.py to get tag and name (?)
-
 	newMember = Member(id, attr, name)
 	DB_Manage.write_member(newMember)
+
 
 ## Untested Commands
 
 ## Get Member Status
 @bot.command()
-async def member_status(ctx, tag):
+async def member_status(ctx, memberTag):
 
-	tag = ctx.author.ID
-	await ctx.send(DB_Manage.get_status(tag))
+	memberID = ctx.author.ID
+	await ctx.send(DB_Manage.get_status(memberID))
 
 ## Start Event Registration 
 @bot.command()
-async def event_registration(ctx, eventName, eventDur):
+async def event_registration(ctx):
 
-	# create blank event, send to aaron
+	# need to add functionality where register button collects id
+	# and send to DB?
+	embed = discord.Embed(
+        title="Registration For 'Add Variable for events here'",
+        color=discord.Color.blue()
+    )
+	embed.add_field(name="**Meeting Area**", value="Blank", inline=False)
+	embed.add_field(name="**Need help?**", value="[Message Blank](https://google.com)", inline=False)
 
-	# Call UI function to display event registration info
-	ui_func_EventRegistrDisplay(eventName, eventDur)
+	view = discord.ui.View()
+	sign_in = discord.ui.Button(label="Sign in", style=discord.ButtonStyle.link, url="https://google.com")
+	sign_out = discord.ui.Button(label="Sign Out", style=discord.ButtonStyle.link, url="https://google.com")
+
+	view.add_item(sign_in)
+	view.add_item(sign_out)
+
+	await ctx.send(embed=embed, view=view)
 
 ## Start Event
 @bot.command()
-async def start_event(ctx, eventName):
+async def start_event(ctx, eventName, startTime, endTime):
+
+	DB_Manage.write_event(eventName, startTime, endTime)
 
 	# Call UI function to end event registration function
 	ui_func_EndRegistration()
@@ -81,10 +106,9 @@ async def start_event(ctx, eventName):
 
 ## Start Meeting
 @bot.command()
-async def start_meeting(ctx, *args):
+async def start_meeting(ctx, eventName, startTime, endTime):
 
-	startTime = current.time
-	# add to db
+	DB_Manage.write_event(eventName, startTime, endTime)
 
 	# Call UI function to start an event with the sign in/out buttons, doesn't need special name
 	ui_func_StartMeeting()
@@ -105,41 +129,46 @@ async def show_profile(ctx, memberTag):
 	memberID = await commands.MemberConverter().convert(ctx, memberTag)
 
 	# Call UI function to display profile
-	ui_func_DisplayProfile(arg1.id)
+	ui_func_DisplayProfile(memberID)
 
-## Manually Chnage Data
+## Manually Change Data
 @bot.command()
-async def edit_data(ctx, memberTag, attrName, newData):
+async def edit_data(ctx, memberTag, attrName, newData, mode = "member", ):
 
 	memberID = await commands.MemberConverter().convert(ctx, memberTag)
+
 	# Call MySQL function to update member databse with given parameters
-	if updateMember(memberTag.id, attrName, newData) == True:
-		await ctx.send("Data successfully changed to " + newData)
-	else:
-		await ctx.send("Manual data change failed, please try again.")
+	DB_Manage.edit_attr(mode, memberID, attrName, newData)
+
+	# if DB_Manage.edit_attr(memberTag.id, attrName, newData) == True:
+	# 	await ctx.send("Data successfully changed to " + newData)
+	# else:
+	# 	await ctx.send("Manual data change failed, please try again.")
 
 ## Edit Event
 @bot.command()
-async def edit_event(ctx, eventName, attrName, newData):
+async def edit_event(ctx, eventName, attrName, newData, mode = "event"):
 
-	if eventName == "Meeting":
+	if eventName.lower() == "meeting":
 		# Assumption that the only thing that could change in meeting is the time?
-		# startTime = newData
 		endTime = 60 + newData
-		updateEvent(eventName, attrName, newData)
-		updateEvent(eventName, endTimeAttributeName, endTime)	
 
 		# Call MySQL function to update event database
-		if updateEvent(eventName, attrName, newData) == True:
-			await ctx.send("Event data successfully changed")
-		else:
-			await ctx.send("Data change failed, please try again.")
+		DB_Manage.edit_attr(mode, eventName.lower(), attrName, newData)
+		DB_Manage.edit_attr(eventName, endTimeAttributeName, endTime)	
+
+	# 	if updateEvent(eventName, attrName, newData) == True:
+	# 		await ctx.send("Event data successfully changed")
+	# 	else:
+	# 		await ctx.send("Data change failed, please try again.")
 	else:
-		# Call MySQL function to update event database
-		if updateEvent(eventName, attrName, newData) == True:
-			await ctx.send("Event data successfully changed")
-		else:
-			await ctx.send("Data change failed, please try again.")
+	 	# Call MySQL function to update event database
+		DB_Manage.edit_attr(mode, eventName.lower(), attrName, newData)	
+
+	# 	if updateEvent(eventName, attrName, newData) == True:
+	# 		await ctx.send("Event data successfully changed")
+	# 	else:
+	# 		await ctx.send("Data change failed, please try again.")
 
 ## Delete Member
 @bot.command()
@@ -148,14 +177,14 @@ async def delete_member(ctx, memberTag):
 	memberID = await commands.MemberConverter().convert(ctx, memberTag)
 
 	# Call MySQL function to delete member
-	remove_member(memberID)
+	DB_Manage.remove_member(memberID)
 
 ## Delete Event
 @bot.command()
 async def delete_event(ctx, eventName):
 
 	# Call MySQL function to delete event
-	remove_event(eventName)
+	DB_Manage.remove_event(eventName)
 
 
 ## Display All Commands
@@ -183,23 +212,3 @@ async def surveyverify(ctx):
         color=discord.Color.blue()
     )
     await ctx.send(embed=embed, view=VerifyView())
-
-
-@bot.command()
-async def register(ctx):
-
-    embed = discord.Embed(
-        title="Registration For 'Add Variable for events here'",
-        color=discord.Color.blue()
-    )
-    embed.add_field(name="**Meeting Area**", value="Blank", inline=False)
-    embed.add_field(name="**Need help?**", value="[Message Blank](https://google.com)", inline=False)
-
-    view = discord.ui.View()
-    sign_in = discord.ui.Button(label="Sign in", style=discord.ButtonStyle.link, url="https://google.com")
-    sign_out = discord.ui.Button(label="Sign Out", style=discord.ButtonStyle.link, url="https://google.com")
-
-    view.add_item(sign_in)
-    view.add_item(sign_out)
-
-    await ctx.send(embed=embed, view=view)
