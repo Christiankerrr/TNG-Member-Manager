3e20b87ac9323294516b8063168071f92f8d35d9
Christian <christiankerr87@gmail.com>
2025-04-21 16:27:58 -0400

    Commands.py

--- Commands.py
+++ Commands.py
@@ -124,25 +124,6 @@ async def surveyverify(ctx):
     )
     await ctx.send(embed=embed, view=VerifyView())
 
-@bot.command()
-async def register(ctx):
-
-    embed = discord.Embed(
-        title="Registration For 'Add Variable for events here'",
-        color=discord.Color.blue()
-    )
-    embed.add_field(name="**Meeting Area**", value="Blank", inline=False)
-    embed.add_field(name="**Need help?**", value="[Message Blank](https://google.com)", inline=False)
-
-    view = discord.ui.View()
-    sign_in = discord.ui.Button(label="Sign in", style=discord.ButtonStyle.link, url="https://google.com")
-    sign_out = discord.ui.Button(label="Sign Out", style=discord.ButtonStyle.link,
-                                       url="https://google.com")
-
-    view.add_item(sign_in)
-    view.add_item(sign_out)
-
-    await ctx.send(embed=embed, view=view)
 
 @bot.command()
 async def surveyverify(ctx):
