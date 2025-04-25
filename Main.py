3dc754ca6621e5c5850f44ad46e7b2259700a600
JHeinis <j.t.heinis@gmail.com>
2025-04-25 06:37:42 -0800

    final push here

--- Main.py
+++ Main.py
@@ -4,14 +4,11 @@
 # pip.main(["install", "discord", "pymysql"])
 
 # import os
-import os
-import time
+# import time
 
 from Commands import bot
-from Event import start_event, end_event, start_meeting
-from Member import Member
 
 if __name__ == "__main__":
 
 
-	bot.run("BOtToken")
+	bot.run("")
